a175dc25f228e75f96925647ba3287981537210d
gaurav16-lang <maihuriagaurav16@gmail.com>
2026-03-07 23:45:08 -0800

    added one more function

--- revision/printAllNumber.js
+++ revision/printAllNumber.js
@@ -1,7 +1,31 @@
-let array = [1, 2, 3, 4, 5, 5, 6];
+let array = [1, -2, 3, -4, 5, -5, 6];
 
 const printAllNumber = (array) => {
   array.map((el) => console.log(el));
 };
 
-printAllNumber(array);
+const countNegativeNumber = (array) => {
+  let count = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < 0) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+console.log(countNegativeNumber(array));
+
+const smallestNumberArray = (array) => {
+  let min = Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+
+  return min;
+};
+
+console.log(smallestNumberArray(array));
